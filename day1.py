b0177be371308584b7b515da2ed45abcd1b07153
abhishek-company-project <abhishek.shantiinfosoft@gmail.com>
2026-03-17 13:25:02 +0530

    Initial commit

---
+++ day1.py
@@ -0,0 +1,38 @@
+
+# print("hello")
+
+# variable
+
+# name1 = "abhishek"
+# first_name="abhishek"
+# firstName="abhishek"
+
+# ra
+
+
+# a=50;
+# b="100";
+
+# print(a+int(b))
+# print(b+str(a))
+
+
+a=4;  # statement
+b=7;
+c=3;
+
+a+=6;  #a=a+6
+print(a)
+
+# print(2**3)
+# print(7**3)
+
+# print(34//4)
+
+print(a%b)
+# print(b%a)
+# print(c%a)
+# print(a%3)
+# print(b%2)
+# print(0%a)
+# print(a%0)
