f3a72c075b33fc2018e952e6664a1231ac9a93e0
abhishek-company-project <abhishek.shantiinfosoft@gmail.com>
2026-03-19 14:29:31 +0530

    dfgs

--- day3.py
+++ day3.py
@@ -1,17 +1,17 @@
 
-x=9;
-y=7;
+# x=9;
+# y=7;
 
-if y%2==1 and x>4:
-    print("x%2")
-    if y<3 or x>5:
-        print(x-y)
-    else:
-        print(y+2)    
-else:
-    print(x-1)
-    if x%2==0 or y%2==1:
-        print("1+2")
+# if y%2==1 and x>4:
+#     print("x%2")
+#     if y<3 or x>5:
+#         print(x-y)
+#     else:
+#         print(y+2)    
+# else:
+#     print(x-1)
+#     if x%2==0 or y%2==1:
+#         print("1+2")
 
 # if x%2==0:
 #     print(x-3)
---
+++ day4.py
@@ -0,0 +1,85 @@
+
+
+# for i in range(4,14,-3):
+#     print("hello")
+
+
+# for i in range(1,9,2):
+#     if i%2==1 and i<7:
+#         print(i-3)
+#         if i%2!=0 or i>6:
+#           print(i) 
+#         else:
+#             print(i-1)  
+#     else:
+#         print(i%2)    
+#     if i%2==0 or i>6:
+#         print(i)    
+
+
+
+
+# for i in range(1,9,2):
+#     if i%2==1 and i<7:
+#         print(i-3)
+#         if i%2!=0 or i>6:
+#           print(i) 
+#         else:
+#             print(i-1)  
+#     else:
+#         print(i%2)    
+#     if i%2==0 or i>6:
+#         print(i)            
+
+
+
+
+
+
+
+# for i in range(2,6):
+#     for j in range(5):
+#        if i%2==0 or i<j:
+#            print("hello")
+#        else:
+#            print(i+j)    
+
+# for i in range(1,6):
+#     for j in range(5):
+#        if i%2==0 and i<j:
+#            print(i%j)
+#            if j>3:
+#                print(j-2)
+#        else:
+#            print(i+j)    
+
+
+
+# i=1;
+# while i<10:
+#     print(i)
+#     if i%2==0:
+#         i+=2;
+#     i+=1;
+
+
+# i=3;
+# while i<10:
+#     print(i)
+#     i+=1;
+#     if i%2==0:
+#         i+=2;
+
+
+
+# i=3;
+# j=5;
+# while i<10 and i!=j:
+#     print(i)
+#     i+=1;
+#     if i%2==0 or j%2==1:
+#         i+=2;
+#         j-=2
+
+
+
